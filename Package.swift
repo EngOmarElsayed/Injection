353a7dbbe2eee82d4b923fc6cad097a91384a716
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2024-07-28 18:49:36 +0300

    Added Macro the package, finished the peer part

--- Package.swift
+++ Package.swift
@@ -1,26 +1,32 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
-    name: "Injection",
-    platforms: [
-        .iOS(.v14),
-        .macOS(.v12),
-        .watchOS(.v5),
-        .tvOS(.v12)
-    ],
-    products: [
-        .library(
-            name: "Injection",
-            targets: ["Injection"]),
-    ],
-    targets: [
-        .target(
-            name: "Injection"),
-        .testTarget(
-            name: "InjectionTests",
-            dependencies: ["Injection"]),
-    ]
+  name: "Injection",
+  platforms: [
+    .iOS(.v14),
+    .macOS(.v10_15),
+    .watchOS(.v5),
+    .tvOS(.v12)
+  ],
+  products: [
+    .library(
+      name: "Injection",
+      targets: ["Injection"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-syntax", from: "510.0.0")
+  ],
+  targets: [
+    .macro(name: "Macros", dependencies: [
+      .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+      .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+    ]),
+    .target(name: "Injection", dependencies: ["Macros"]),
+    .testTarget(name: "InjectionTests", dependencies: ["Injection"]),
+    .testTarget(name: "MacrosTest", dependencies: ["Macros", .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")])
+  ]
 )
