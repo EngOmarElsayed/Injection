ec1b9c031418e8f71db59d4aa294697ac70c59a2
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2024-07-28 23:02:50 +0300

    added copy right notice

--- Tests/InjectionTests/InjectionTests.swift
+++ Tests/InjectionTests/InjectionTests.swift
@@ -1,3 +1,10 @@
+//Copyright (c) 2024 Eng.Omar Elsayed
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
 //furnished to do so, subject to the following conditions:
 //
 //The above copyright notice and this permission notice shall be included in all
@@ -64,4 +71,5 @@ fileprivate struct NetworkProviderMock: Network {
 
 @InjecteValues extension InjectedValues {
   fileprivate var networkProvider: Network = NetworkProvider()
+  fileprivate var test: String = "Test"
 }
